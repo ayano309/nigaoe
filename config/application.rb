b2dcbe6e8124fe8a535201acfd6c6942569db676
綾乃 <>
2022-07-01 10:59:56 +0900

    [install]gem

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module Nigaoe
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
